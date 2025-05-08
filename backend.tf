506281fed74dac0cd08325ab7de5da90b064fd02
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-05-08 11:56:00 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
-    bucket = "backenderyrtur"     # Replace with your bucket
+    bucket = "backenderyrtur"        # 🔧 Replace with your S3 bucket name
     key    = "image-resizing/terraform.tfstate"
     region = "ap-south-1"
     encrypt = true
   }
+}
