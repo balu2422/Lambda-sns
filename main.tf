b065356fd8d211dc8aca1023f11e71a1e5598ef5
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-05-08 12:36:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,27 @@
-# main.tf
-
 provider "aws" {
   region = "ap-south-1"
 }
 
-# Including all modules
 module "s3" {
-  source = "./modules/s3"
-  ...
-}
-
-module "iam" {
-  source = "./modules/iam"
-  ...
+  source      = "./modules/s3"
+  bucket_name = var.bucket_name
 }
 
 module "sns" {
-  source = "./modules/sns"
-  ...
+  source         = "./modules/sns"
+  topic_name     = var.topic_name
+  email_endpoint = var.email_endpoint
+}
+
+module "iam" {
+  source      = "./modules/iam"
+  lambda_name = var.lambda_name
 }
 
 module "lambda" {
-  source = "./modules/lambda"
-  ...
+  source           = "./modules/lambda"
+  lambda_name      = var.lambda_name
+  bucket_name      = var.bucket_name
+  sns_topic_arn    = module.sns.topic_arn
+  lambda_role_arn  = module.iam.lambda_role_arn
 }
