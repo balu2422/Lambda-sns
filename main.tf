185394f161d3ead8c9eeafc0d83cbe6b8227adeb
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-05-08 12:53:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,26 +2,28 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# S3 Module: Defining Buckets (No parameters passed as it's already handled inside the module)
 module "s3" {
-  source      = "./modules/s3"
-  bucket_name = var.bucket_name
+  source = "./modules/s3"
 }
 
+# SNS Module: Topic and Subscription
 module "sns" {
   source         = "./modules/sns"
-  topic_name     = var.topic_name
-  email_endpoint = var.email_endpoint
+  topic_name     = "image-processing-topic"  # Hardcoded topic name
+  email_endpoint = "tippuluri1998@gmail.com"  # Hardcoded email subscription
 }
 
+# IAM Module: Lambda Execution Role
 module "iam" {
   source      = "./modules/iam"
-  lambda_name = var.lambda_name
+  lambda_name = "image-processor-lambda"  # Hardcoded Lambda function name
 }
 
+# Lambda Module: Image Resizing Lambda Function
 module "lambda" {
   source           = "./modules/lambda"
-  lambda_name      = var.lambda_name
-  bucket_name      = var.bucket_name
-  sns_topic_arn    = module.sns.topic_arn
-  lambda_role_arn  = module.iam.lambda_role_arn
+  lambda_name      = "image-processor-lambda"  # Hardcoded Lambda function name
+  sns_topic_arn    = module.sns.sns_topic_arn  # SNS topic ARN from sns module
+  lambda_role_arn  = module.iam.lambda_exec_role_arn  # IAM role ARN from iam module
 }
