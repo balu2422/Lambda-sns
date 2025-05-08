22eed54e0fe128462c4d67fbcee3f1d2d831d9cc
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-05-08 12:18:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,17 +6,17 @@ provider "aws" {
 
 # Including all modules
 module "s3" {
-  source = "./modules/s3"
+  source = "./modules/s3.tf"
 }
 
 module "iam" {
-  source = "./modules/iam"
+  source = "./modules/iam.tf"
 }
 
 module "lambda" {
-  source = "./modules/lambda"
+  source = "./modules/lambda.tf"
 }
 
 module "sns" {
-  source = "./modules/sns"
+  source = "./modules/sns.tf"
 }
